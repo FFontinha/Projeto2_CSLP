b6f4fde10194cf92fbe8633ba1f1a9eb55b1d919
Catarina10Borges <c.pintoborges@ua.pt>
2019-10-25 10:19:45 +0100

    Merge branch 'master' of /home/catarina/Desktop/CSLP/projeto2/Projeto2_CSLP with conflicts.

--- image.h
+++ image.h
@@ -1,43 +1,35 @@
 //
-// Created by francisco on 10/17/19.
+// Created by catarina on 10/17/19.
 //
 
 #ifndef IMAGES_IMAGE_H
 #define IMAGES_IMAGE_H
 
-
-
-
-
-typedef struct  {
-    unsigned char r,g,b; // 0-255
-} RGBPixel;
-
-typedef  struct {
-    unsigned char gray; // 0-255
-} GrayPixel;
-
-typedef struct {
-    unsigned char bin; // 0 or 1
-} BinPixel;
-
-typedef struct {
+typedef struct
+{
     int w, h;
-    RGBPixel *dataRGB;
-    GrayPixel *dataGray;
-    BinPixel *dataBin;
+    unsigned char type;
 
 } Image;
 
+typedef struct
+{
+    unsigned char r,g,b; // 0-255
+} RGBPixel;
 
+typedef  struct
+{
+    unsigned char gray; // 0-255
+} GrayPixel;
+
+typedef struct
+{
+    unsigned char bin; // 0 or 1
+} BinPixel;
 
 //Image * CreateImage(int w, int h);
 
 Image * LoadFromFile(char *);
-
-void changeColorPPM(Image *img);
-void toGrey(Image *img);
-void writeGrey(const char *filename, Image *img);
 
 #endif //IMAGES_IMAGE_H
 
